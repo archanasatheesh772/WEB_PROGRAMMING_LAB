2a1ed91b5e23af6378757ccf153df6fa291ec305
ARCHANA SATHEESH <archanasatheesh772@gmail.com>
2025-11-15 01:45:02 -0800

    Add files via upload

---
+++ asort.php
@@ -0,0 +1,13 @@
+<?php
+$a = array("anjala","christeena","blessy","angela");
+
+print_r($a);
+
+echo "<br>Ascending order: ";
+asort($a);
+print_r($a);
+
+echo "<br>Descending order: ";
+arsort($a);
+print_r($a);
+?>
